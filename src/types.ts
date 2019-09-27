281fb564493f507ef21bec7049142dccab1a3d0c
Luke <lbrobbin@jaguarlandrover.com>
2019-09-27 21:47:43 +0100

    add checkout kata

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,20 @@ export interface IPickPeaksReturn {
   peaks: number[];
   pos: number[];
 }
+
+export type priceItems = "A" | "B" | "C" | "D" | string;
+
+export interface IPriceSystem {
+  [key: string]: IPriceSystemItem;
+  A: IPriceSystemItem;
+  B: IPriceSystemItem;
+  C: IPriceSystemItem;
+  D: IPriceSystemItem;
+}
+
+export interface IPriceSystemItem {
+  price: number;
+  total: number;
+  offerPrice: number;
+  numForOffer: number;
+}
