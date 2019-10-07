2bd8bf722f96b35483068d9f30784aa8871356e9
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-10-07 13:05:10 +0200

    add flatten and reflect

--- src/katas/reflect.ts
+++ src/katas/reflect.ts
@@ -4,7 +4,7 @@ import { IReflectReturnVals } from "../types";
  *
  * @param promise a promise
  * @description Given a promise, should return an object with a `'fulfilled'` status, as well as the value,
- *              and an object with a `'rejected'` status and a reason:
+ *              or an object with a `'rejected'` status and a reason:
  *              - resolved promise - `{status: "fulfilled", value: [1,2,3]}`
  *              - rejected promise - `{status: "rejected", reason: "Server Error"}`
  *
